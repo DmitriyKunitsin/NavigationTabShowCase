307a395a46ac14c6a10025e5c9d7cac86e81c1fe
DmitryKunitsin <kunicind1996@gmail.com>
2025-08-26 16:42:26 +0700

    .

--- DesignViewModel/DesignActionStrimViewModel.cs
+++ DesignViewModel/DesignActionStrimViewModel.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections.ObjectModel;
+﻿using NavigationTabShowCase.ViewModels;
+using System.Collections.ObjectModel;
 
 namespace NavigationTabShowCase.DesignViewModel
 {
-    public static class DesignActionStrimViewModel 
+    public static class DesignActionStrimViewModel
     {
-        public static ObservableCollection<string> Actions { get;  } = new ObservableCollection<string>() { "Главная", "Файл", "Меню" };
+        public static ObservableCollection<string> Actions { get; } = new ObservableCollection<string>() { "Главная", "Файл", "Меню" };
+        public static ObservableCollection<ActionViewModel> ActionsViewModels { get; } = new ObservableCollection<ActionViewModel>()
+        {
+            new ActionViewModel() {Title = "Главная"},
+            new ActionViewModel() {Title = "Файл"},
+            new ActionViewModel() {Title = "Данные"},
+        };
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,23 +1,18 @@
-﻿using NavigationTabShowCase.Controls;
-using NavigationTabShowCase.ViewModels.Intarfaces;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
 namespace NavigationTabShowCase.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public ObservableCollection<ActionViewModel> Tabs { get; }
-
-        public MainWindowViewModel()
-        {
-            Tabs = new ObservableCollection<ActionViewModel>() 
-            { 
+        public ObservableCollection<ActionViewModel> Tabs { get; } = new ObservableCollection<ActionViewModel>()
+            {
                 new ActionViewModel() {Title = "Главная"},
                 new ActionViewModel() {Title = "Файл"},
                 new ActionViewModel() {Title = "Данные"},
-            } ;
+            };
+
+        public MainWindowViewModel()
+        {
         }
     }
 }
