f7150cea7371ab0de6d4e6c83bb49bab9637872c
DmitryKunitsin <kunicind1996@gmail.com>
2025-08-28 15:01:59 +0700

    add styled for btn in StipItem

--- Controls/NavigationActionStripItem.cs
+++ Controls/NavigationActionStripItem.cs
@@ -4,40 +4,70 @@ using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using MsBox.Avalonia;
-using Avalonia.Interactivity;
-using NavigationTabShowCase.ViewModels.Intarfaces;
-using System;
-using System.Windows.Input;
-using System.Threading.Tasks;
 
 namespace NavigationTabShowCase.Controls
 {
     [TemplatePart(TpTextBlock, typeof(TextBlock))]
+    [PseudoClasses(PcSelected)]
     public class NavigationActionStripItem : Button
     {
         public NavigationActionStripItem() { }
         private TextBlock? _textBlock;
         public const string TpTextBlock = "PART_TextBlock";
+        public const string PcSelected = ":selectedBtn";
+        private bool _isSelected;
 
-        public static readonly StyledProperty<string> TitleProperty = 
+        public static readonly StyledProperty<string> TitleProperty =
             AvaloniaProperty.Register<NavigationActionStripItem, string>(
-                name: nameof(Title), 
+                name: nameof(Title),
                 defaultValue: string.Empty);
-        public string Title 
-        { 
-            get => GetValue( TitleProperty ); 
-            set => SetValue(TitleProperty, value); 
+        public string Title
+        {
+            get => GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
         }
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             base.OnApplyTemplate(e);
             _textBlock = e.NameScope.Find<TextBlock>(TpTextBlock);
+            SetPseudoClasses();
         }
-
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    SetPseudoClasses();
+                }
+            }
+        }
+        private void SetPseudoClasses()
+        {
+            PseudoClasses.Set(PcSelected, IsSelected);
+        }
+        protected override void OnPointerEntered(PointerEventArgs e)
+        {
+            base.OnPointerEntered(e);
+            if (!_isSelected)
+            {
+                IsSelected = true;
+            }
+        }
+        protected override void OnPointerExited(PointerEventArgs e)
+        {
+            base.OnPointerExited(e);
+            if (_isSelected)
+            {
+                IsSelected = false;
+            }
+        }
         protected override void OnPointerPressed(PointerPressedEventArgs e)
         {
             base.OnPointerPressed(e);
-            if(e.Source is TextBlock b)
+            if (e.Source is TextBlock b)
             {
                 var box = MessageBoxManager.GetMessageBoxStandard($"MsBox", $"{b.Text}", @enum: MsBox.Avalonia.Enums.ButtonEnum.Ok);
                 var res = box.ShowAsync(); // Да да я знаю, что тут надо await
--- DesignViewModel/DesignActionStrimViewModel.cs
+++ DesignViewModel/DesignActionStrimViewModel.cs
@@ -8,9 +8,9 @@ namespace NavigationTabShowCase.DesignViewModel
         public static ObservableCollection<string> Actions { get; } = new ObservableCollection<string>() { "Главная", "Файл", "Меню" };
         public static ObservableCollection<ActionViewModel> ActionsViewModels { get; } = new ObservableCollection<ActionViewModel>()
         {
-            new ActionViewModel() {Description = "Главная ActionsViewModels"},
-            new ActionViewModel() {Description = "Файл ActionsViewModels"},
-            new ActionViewModel() {Description = "Данные ActionsViewModels"},
+            new ActionViewModel() {Description = "Главная"},
+            new ActionViewModel() {Description = "Файл"},
+            new ActionViewModel() {Description = "Данные"},
         };
     }
 }
